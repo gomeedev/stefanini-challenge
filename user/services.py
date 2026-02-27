a58257bd137f1a4c39a1a45d7cda8d66cd00a53c
gomeedev <johanns.gomeze@gmail.com>
2026-02-27 12:49:39 -0500

    refactor(user): Change traditional validation to http in the service layer

--- user/services.py
+++ user/services.py
@@ -1,4 +1,4 @@
-from django.core.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError
 from .models import User
 
 
@@ -6,7 +6,7 @@ from .models import User
 # Harold, no validé formato de correo porque django internamente ya lo hace
 def create_user(email:str, first_name:str, last_name:str):
     if User.objects.filter(email=email).exists():
-        raise ValidationError("Este correo ya existe, ingresa uno diferente")
+        raise ValidationError({"email": "Este correo ya existe, ingresa uno diferente"})
     
     return User.objects.create(
         # Igualé 'username' con email porque django me obliga y porque no implementaré login
