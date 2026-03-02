9c2ea28e36438fbf9b60c90a2dbc2e45d843059f
gomeedev <johanns.gomeze@gmail.com>
2026-03-02 01:04:46 -0500

    feat(assistant): add consultation endpoint with session support

--- assistant/views.py
+++ assistant/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .services import AssistantService
+
+
+
+
+@api_view(['POST'])
+def consult_assistan(request):
+    
+    question = request.data.get('pregunta')
+    user_id  = request.data.get('user_id') 
+    session_id = request.data.get('session_id') 
+    
+    if not question:
+        return Response(
+            {
+                "error": "Debes enviar una 'pregunta' en el body",
+            }, status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    if not session_id:
+        return Response(
+            {"error": "Debes enviar un session_id"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    
+    service = AssistantService()
+    result = service.consult(session_id, question, user_id=user_id)
+    
+    if result['success']:
+        return Response({
+            "pregunta": question,
+            "respuesta": result['respuesta'],
+        }, status=status.HTTP_200_OK)
+    else:
+        return Response(
+            {
+                "error": result['error']
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
